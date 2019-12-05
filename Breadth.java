322cb13c4cfe824ab0afa4fe147b0f1e45c68b1d
Abdalikhan <52061303+Abdalikhan@users.noreply.github.com>
2019-12-04 19:16:36 -0800

    Update Breadth.java

--- Breadth.java
+++ Breadth.java
@@ -5,29 +5,6 @@ import java.util.Queue;
 public class Breadth {
 	public static void main(String[] args) {
 		String rootDir = "/users/Abdal/documents";
-		/*File largestFile = null;
-		Queue<File> queue = new LinkedList<>();
-		queue.add(new File(rootDir));
-		while (!queue.isEmpty())
-		{
-			File current = queue.poll();
-			File[] listOfFilesAndDirectory = current.listFiles();
-			if(listOfFilesAndDirectory != null)
-			{
-				for (File file : listOfFilesAndDirectory)
-				{
-					if(file.isDirectory()) {
-						queue.add(file);
-					}
-					else {
-							if (largestFile == null || file.length() > largestFile.length())
-								largestFile = file;
-							System.out.println(file);
-						}
-					}
-					
-				}
-		}*/
 		File largestFile = findLargest(rootDir);
 		if (largestFile != null)
 			System.out.println("Largest file was: " + largestFile);
